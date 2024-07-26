6f03db39096e6750b241f23476bdb16cf9ea4535
Ak norinori <42559915+nakayamaakinori0@users.noreply.github.com>
2024-07-26 18:15:42 +0900

    Update main.ts

--- bingoGame/main.ts
+++ bingoGame/main.ts
@@ -1,7 +1,6 @@
 process.stdin.resume();
 process.stdin.setEncoding("utf8");
-// 自分の得意な言語で
-// Let's チャレンジ！！
+
 var lines: string[] = [];
 var reader = require("readline").createInterface({
   input: process.stdin,
